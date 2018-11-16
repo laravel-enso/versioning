cf98c10846f88f81c6fd4fa96db6e37c54fafd10
Mihai <mihai.ocneanu@gmail.com>
2018-11-16 16:41:41 +0200

    fixed small bug on updating, when relationshio was not loaded

--- src/app/Traits/Versionable.php
+++ src/app/Traits/Versionable.php
@@ -37,7 +37,7 @@ trait Versionable
         self::updating(function ($model) {
             \DB::beginTransaction();
 
-            if (! isset($model->{$model->versioningAttribute()})) {
+            if (!isset($model->{$model->versioningAttribute()})) {
                 \DB::rollback();
 
                 throw new ConflictHttpException(__(
@@ -50,7 +50,7 @@ trait Versionable
                 ->lockForUpdate()
                 ->first();
 
-            if (! $versioning) {
+            if (!$versioning) {
                 \DB::rollback();
 
                 throw new ConflictHttpException(__(
@@ -72,9 +72,11 @@ trait Versionable
         });
 
         self::updated(function ($model) {
-            $model->versioning()
-                ->first()
-                ->increment('version');
+            if(! $model->versioning) {
+                $model->load('versioning');
+            }
+
+            $model->versioning->increment('version');
 
             $model->{$model->versioningAttribute()} = $model->versioning->version;
 
@@ -84,10 +86,10 @@ trait Versionable
         });
 
         self::deleted(function ($model) {
-            if (! in_array(SoftDeletes::class, class_uses(get_class($model)))
+            if (!in_array(SoftDeletes::class, class_uses(get_class($model)))
                 || $model->isForceDeleting()) {
                 $model->versioning()
-                        ->delete();
+                    ->delete();
             }
         });
     }
@@ -97,18 +99,18 @@ trait Versionable
         return $this->morphOne(Versioning::class, 'versionable');
     }
 
-    private function startVersioning()
-    {
-        $this->versioning()
-                ->save(new Versioning());
-
-        $this->{$this->versioningAttribute()} = 1;
-    }
-
     private function versioningAttribute()
     {
         return property_exists($this, 'versioningAttribute')
-                ? $this->versioningAttribute
-                : 'version';
+            ? $this->versioningAttribute
+            : 'version';
+    }
+
+    private function startVersioning()
+    {
+        $this->versioning()
+            ->save(new Versioning());
+
+        $this->{$this->versioningAttribute()} = 1;
     }
 }
