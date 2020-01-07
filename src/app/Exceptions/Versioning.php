23df862522c99cb95a99346cb4a236a136c82951
mishu <mishu@xtelecom.ro>
2020-01-07 16:05:53 +0200

    refactors exception to extends ConflictHttpException

--- src/app/Exceptions/Versioning.php
+++ src/app/Exceptions/Versioning.php
@@ -3,12 +3,12 @@
 namespace LaravelEnso\Versioning\App\Exceptions;
 
 use LaravelEnso\Helpers\App\Exceptions\EnsoException;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 
-class Versioning extends EnsoException
+class Versioning extends ConflictHttpException
 {
     public static function recordModified()
     {
-        return new static(__('Current record was changed since it was loaded'),
-            409);
+        return new static(__('Current record was changed since it was loaded'));
     }
 }
